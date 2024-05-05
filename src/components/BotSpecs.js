24c252ad542a27ce70708480177cb2426b1609c2
kelvindev1 <kelvinmutugi336@gmail.com>
2024-05-05 14:53:17 +0300

    css style

--- src/components/BotSpecs.js
+++ src/components/BotSpecs.js
@@ -20,18 +20,20 @@ function BotSpecs({ onEnlist, onBack }) {
     );
   }
   return (
-    <div id="BotSpecs-container">
-      <img src={bot.avatar_url} alt="Bot Avatar" />
-      <h1>{bot.name}</h1>
-      <p>Catchphrase: {bot.catchphrase}</p>
-      <p>Health: {bot.health}</p>
-      <p>Damage: {bot.damage}</p>
-      <p>Armor: {bot.armor}</p>
-      <p>Bot Class: {bot.bot_class}</p>
-      <p>Created At: {bot.created_at}</p>
-      <p>Updated At: {bot.updated_at}</p>
-      <button id="">Enlist Bot</button>
-      <button id="listbtn">Back to List View</button>
+    <div className="BotSpecs-container">
+      <div className="Botitems">
+        <img src={bot.avatar_url} alt="Bot Avatar" />
+        <h1>{bot.name}</h1>
+        <p>Catchphrase: {bot.catchphrase}</p>
+        <p>Health: {bot.health}</p>
+        <p>Damage: {bot.damage}</p>
+        <p>Armor: {bot.armor}</p>
+        <p>Bot Class: {bot.bot_class}</p>
+        <p>Created At: {bot.created_at}</p>
+        <p>Updated At: {bot.updated_at}</p>
+        <button className="enlistbtn">Enlist Bot</button>
+        <button className="backbtn">Back to List View</button>
+      </div>
     </div>
   );
 }
